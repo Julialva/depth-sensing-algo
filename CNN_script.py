76051a9f499d34ce50dd5acc7c98c8526c3d4732
gabidom <gabriel@fdominguez.com.br>
2022-09-29 22:13:24 -0300

    coisas

--- CNN_script.py
+++ CNN_script.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras import optimizers
@@ -14,36 +15,41 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from calibrator import calibrate, undistort
 import cv2
 logging.basicConfig(level=logging.INFO,
-    format='%(asctime)s - %(funcName)s - %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+                    format='%(asctime)s - %(funcName)s - %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 logging.info(f"done importing... tf version:{tf.__version__}")
 logging.info(f"{tf.config.list_physical_devices('GPU')}")
-h=360
-w=640
+h = 360
+w = 640
 img_size = (h, w)
-zerao = np.zeros((h,w))
+zerao = np.zeros((h, w))
 # Define diretório onde se encontram as imagens
 left_image_path = './Final_pics/left'
 right_image_path = './Final_pics/right'
-logging.info('getting paths...')    
+logging.info('getting paths...')
 # Escolhe tipos de arquivos desejados
 glob_left_imgs = os.path.join(left_image_path, '*.png')
 glob_right_imgs = os.path.join(right_image_path, '*.png')
 
-def load_img_dir(dir:str,ret, mtx, dist, rvecs, tvecs):
-    return [tf.image.resize(undistort(ret, mtx, dist, rvecs, tvecs,(640, 360),img_to_array(load_img(file)))[:, :, :3], img_size)/255. for file in glob.glob(dir)]
 
-def load_calib_dir(dir:str):
+def load_img_dir(dir: str, ret, mtx, dist, rvecs, tvecs):
+    return [tf.image.resize(undistort(ret, mtx, dist, rvecs, tvecs, (640, 360), img_to_array(load_img(file)))[:, :, :3], img_size)/255. for file in glob.glob(dir)]
+
+
+def load_calib_dir(dir: str):
     return [cv2.imread(file) for file in glob.glob(dir)]
+
 
 def call_calibrate():
     left_calib_imgs = load_calib_dir('./calib/left_cal/*.jpeg')
     right_calib_imgs = load_calib_dir('./calib/right_cal/*.jpeg')
-    return calibrate(left_calib_imgs),calibrate(right_calib_imgs)
+    return calibrate(left_calib_imgs), calibrate(right_calib_imgs)
+
+
 # Cria lista dos nomes dos arquivos
-calib_l,calib_r= call_calibrate()
+calib_l, calib_r = call_calibrate()
 logging.info("loading imgs...")
-left_imgs = load_img_dir(glob_left_imgs,*calib_l)
-right_imgs = load_img_dir(glob_right_imgs,*calib_r)
+left_imgs = load_img_dir(glob_left_imgs, *calib_l)
+right_imgs = load_img_dir(glob_right_imgs, *calib_r)
 
 left_imgs, right_imgs = shuffle(left_imgs, right_imgs)
 logging.info("loaded DS!")
@@ -63,6 +69,8 @@ m_val = len(val_left_imgs)
 logging.info("splitted DS!")
 
 # Classe para reconstrutor
+
+
 def batch_generator(left_imgs, right_imgs, img_size, m_exemplos, batchsize):
     # Inicializa loop infinito que termina no final do treinamento
     while True:
@@ -86,6 +94,7 @@ def batch_generator(left_imgs, right_imgs, img_size, m_exemplos, batchsize):
 
             yield [np.stack(batch_left_img, axis=0), np.stack(batch_right_img, axis=0)], [np.stack(batch_out_img, axis=0), np.stack(batch_disp_img, axis=0)]
             # Não tenho certeza se deixa o [] na saída
+
 
 class Reconstructor(tf.keras.layers.Layer):
     def __init__(self, height=40, width=40, num_channels=3, name="rec_img"):
@@ -194,50 +203,53 @@ rec = Reconstructor(
     height=img_size[0], width=img_size[1])
 
 
-from tensorflow.keras import models
-from tensorflow.keras import layers
-
 # Número de filtros básico
 nF = 48
 
 # Rede convolucional de extração de características
+
+
 def rna_carac(input_shape, nF):
     x0 = layers.Input(shape=input_shape)
-    
-    x1 = layers.Conv2D(nF, (5,5), padding='same', activation=layers.LeakyReLU())(x0)
-    x2 = layers.Conv2D(nF, (5,5), padding='same', use_bias=False)(x1)
+
+    x1 = layers.Conv2D(nF, (5, 5), padding='same',
+                       activation=layers.LeakyReLU())(x0)
+    x2 = layers.Conv2D(nF, (5, 5), padding='same', use_bias=False)(x1)
     x2 = layers.BatchNormalization()(x2)
     x2 = layers.LeakyReLU()(x2)
-    
-    x3 = layers.MaxPool2D(2,2)(x2)
-    x3 = layers.Conv2D(nF*2, (5,5), padding='same', activation=layers.LeakyReLU())(x3)
-    
-    x4 = layers.Conv2D(nF*2, (5,5), padding='same', use_bias=False)(x3)
+
+    x3 = layers.MaxPool2D(2, 2)(x2)
+    x3 = layers.Conv2D(nF*2, (5, 5), padding='same',
+                       activation=layers.LeakyReLU())(x3)
+
+    x4 = layers.Conv2D(nF*2, (5, 5), padding='same', use_bias=False)(x3)
     x4 = layers.BatchNormalization()(x4)
     x4 = layers.LeakyReLU()(x4)
-    
-    x5 = layers.MaxPool2D(2,2)(x4)
-    x5 = layers.Conv2D(nF*4, (5,5), padding='same', activation=layers.LeakyReLU())(x5)
-   
-    x6 = layers.Conv2D(nF*4, (5,5), padding='same', use_bias=False)(x5)
-    x6 = layers.BatchNormalization()(x6)
-    x6 = layers.LeakyReLU()(x6) 
 
-    x7 = layers.MaxPool2D(2,2)(x6)  
-    x7 = layers.Conv2D(nF*4, (5,5), padding='same', activation=layers.LeakyReLU())(x7)
-    
-    x8 = layers.Conv2D(nF*4, (5,5), padding='same', use_bias=False)(x7)
+    x5 = layers.MaxPool2D(2, 2)(x4)
+    x5 = layers.Conv2D(nF*4, (5, 5), padding='same',
+                       activation=layers.LeakyReLU())(x5)
+
+    x6 = layers.Conv2D(nF*4, (5, 5), padding='same', use_bias=False)(x5)
+    x6 = layers.BatchNormalization()(x6)
+    x6 = layers.LeakyReLU()(x6)
+
+    x7 = layers.MaxPool2D(2, 2)(x6)
+    x7 = layers.Conv2D(nF*4, (5, 5), padding='same',
+                       activation=layers.LeakyReLU())(x7)
+
+    x8 = layers.Conv2D(nF*4, (5, 5), padding='same', use_bias=False)(x7)
     x8 = layers.BatchNormalization()(x8)
-    x8 = layers.LeakyReLU()(x8) 
+    x8 = layers.LeakyReLU()(x8)
 
     # Cria modelo
     rna_carac = models.Model(x0, [x2, x4, x6, x8])
-    
+
     return rna_carac
+
 
 input_shape = (360, 640, 3)
 rnaCV = rna_carac(input_shape, nF)
-
 
 
 """## Rede completa"""
@@ -254,31 +266,35 @@ y1R, y2R, y3R, y4R = rnaCV(input_right)
 # Cocatena com características 4
 ac2 = layers.Concatenate(axis=-1)([y4L, y4R])
 
-# Aplica convolução 
-x2 = layers.Conv2DTranspose(64, (3,3), padding='same', activation='relu', strides=(2,2))(ac2)
-x2 = layers.Conv2D(64, (3,3), padding='same', activation='relu')(x2)
+# Aplica convolução
+x2 = layers.Conv2DTranspose(
+    64, (3, 3), padding='same', activation='relu', strides=(2, 2))(ac2)
+x2 = layers.Conv2D(64, (3, 3), padding='same', activation='relu')(x2)
 
 # Cocatena com características 3
 ac3 = layers.Concatenate(axis=-1)([y3L, y3R, x2])
 
-# Aplica convolução 
-x3 = layers.Conv2DTranspose(32, (3,3), padding='same', activation='relu', strides=(2,2))(ac3)
-x3 = layers.Conv2D(32, (3,3), padding='same', activation='relu')(x3)
+# Aplica convolução
+x3 = layers.Conv2DTranspose(
+    32, (3, 3), padding='same', activation='relu', strides=(2, 2))(ac3)
+x3 = layers.Conv2D(32, (3, 3), padding='same', activation='relu')(x3)
 
 # Cocatena com características 2
 ac4 = layers.Concatenate(axis=-1)([y2L, y2R, x3])
 
 # Aplica convolução
-x4 = layers.Conv2DTranspose(16, (3,3), padding='same', activation='relu', strides=(2,2))(ac4)
-x4 = layers.Conv2D(16, (3,3), padding='same', activation='relu')(x4)
+x4 = layers.Conv2DTranspose(
+    16, (3, 3), padding='same', activation='relu', strides=(2, 2))(ac4)
+x4 = layers.Conv2D(16, (3, 3), padding='same', activation='relu')(x4)
 
 # Cocatena com características 1
 ac5 = layers.Concatenate(axis=-1)([y1L, y1R, x4])
 
 # Aplica convolução e calcula disparidade
-x5 = layers.Conv2D(16, (3,3), padding='same', activation='relu')(ac5)
-disp = layers.Conv2D(1, (1,1), padding='same', activation='relu', name='disp')(x5)
-#print(disp.shape)
+x5 = layers.Conv2D(16, (3, 3), padding='same', activation='relu')(ac5)
+disp = layers.Conv2D(1, (1, 1), padding='same',
+                     activation='relu', name='disp')(x5)
+# print(disp.shape)
 
 # Imagem da direita reconstruída usando a imagem esquerda e a disparidade
 img_rec = rec(input_left, disp)
@@ -286,10 +302,11 @@ img_rec = rec(input_left, disp)
 # Prepara saidas
 #images_out_layer = layers.Lambda(lambda x: tf.concat([x[0], x[1]], -1), name='img_out')
 #images_out = images_out_layer([img_rec, input_left])
-#print(images_out.shape)
+# print(images_out.shape)
 
 # Create the Keras model.
-rna_stereo = keras.Model(inputs=[input_left, input_right], outputs=[img_rec, disp])
+rna_stereo = keras.Model(
+    inputs=[input_left, input_right], outputs=[img_rec, disp])
 
 
 # Define otimizador Adam
@@ -309,7 +326,7 @@ rna_stereo.compile(optimizer=adam,
 
 
 # Importa callback para salvar modelo durante treinamento
-
+rna_stereo.load_weights("./ckpts")
 # Define o callback para salvar os parâmetros
 checkpointer = ModelCheckpoint(
     'rna_stereo_CVN_REC_weigths', verbose=1, save_best_only=True, save_weights_only=True)
@@ -318,13 +335,14 @@ train_steps = len(train_left_imgs) // batch_size
 val_steps = len(val_left_imgs) // batch_size
 
 
-results = rna_stereo.fit(batch_generator(train_left_imgs, train_right_imgs,img_size, m_train, batch_size),
-    steps_per_epoch=train_steps,
-    epochs=1000,
-    validation_data=batch_generator(val_left_imgs, val_right_imgs, img_size, m_val, batchsize=batch_size),
-    callbacks=[checkpointer],
-    validation_steps=val_steps,
-    verbose=1)
+results = rna_stereo.fit(batch_generator(train_left_imgs, train_right_imgs, img_size, m_train, batch_size),
+                         steps_per_epoch=train_steps,
+                         epochs=1000,
+                         validation_data=batch_generator(
+                             val_left_imgs, val_right_imgs, img_size, m_val, batchsize=batch_size),
+                         callbacks=[checkpointer],
+                         validation_steps=val_steps,
+                         verbose=1)
 
 
 # Restore the weights
@@ -339,10 +357,11 @@ hist_json_file = 'history.json'
 with open(hist_json_file, mode='w') as f:
     df_hist.to_json(f)
 
-import matplotlib.pyplot as plt
-datagen=batch_generator(train_left_imgs, train_right_imgs,img_size, m_train, batch_size)
-[left_img_batch, right_img_batch], [out_img_batch,__] = next(datagen)
-img_prev_batch,disp_prev_batch = rna_stereo.predict([left_img_batch, right_img_batch])
+datagen = batch_generator(
+    train_left_imgs, train_right_imgs, img_size, m_train, batch_size)
+[left_img_batch, right_img_batch], [out_img_batch, __] = next(datagen)
+img_prev_batch, disp_prev_batch = rna_stereo.predict(
+    [left_img_batch, right_img_batch])
 
 # Seleciona exemplo para análise
 index = 3
@@ -363,4 +382,4 @@ pos[1].imshow(img_prev)
 pos[2].imshow(disp_prev, cmap='gray')
 plt.savefig('plot1.jpeg')
 
-plt.imsave("disp_img.jpeg",disp_prev, cmap='gray')
+plt.imsave("disp_img.jpeg", disp_prev, cmap='gray')
